1c7b47a4add856014185155e0253838fbe02eedb
Elilucio <elilucio.filho@aluno.ufca.edu.br>
2026-03-18 18:12:56 -0300

    Update cli_update_commands.py

--- cli/commands/cli_update_commands.py
+++ cli/commands/cli_update_commands.py
@@ -2,7 +2,7 @@ from database.querys.sql_updates import SQLUpdate
 
 class CLIUpdateCommand:
 
-    def __int__(self):
+    def __init__(self):
         self.repo = SQLUpdate()
 
     def cli_update_compaint_on_audience(self):
@@ -11,8 +11,7 @@ class CLIUpdateCommand:
         audience_id = int(input("Id da audiência: "))
 
         try:
-            up = UpdateComp(complaint_id=complaint_id, audience_id=audience_id)
-
+            up = self.repo.update_complaint_on_audience(complaint_id=complaint_id, audience_id=audience_id)
         except Exception as error:
             return "Erro ao atualizar id"
         return "id da audiência atualizado com sucesso"
@@ -22,10 +21,8 @@ class CLIUpdateCommand:
         name = str(input("Nome novo: "))
         phone = str(input("Telefone novo: "))
         pessoa_id = int(input("Digite seu identificador no sistema para confirmar as alterações: \n"))
-
         try:
-             up = UpdatePerson(name=name, phone=phone, pessoa_id=pessoa_id)
-             up.update()
+             up = self.repo.update_person(name=name, phone=phone, pessoa_id=pessoa_id)
         except Exception as error:
             return "Erro ao atualizar dados"
         return "Dados atualizados."
@@ -35,10 +32,10 @@ class CLIUpdateCommand:
         status_acordo = str(input("Novo status: "))
         data_acordo = input("Data do acordo: ")
         acordo_id = int(input("Digite o id do acordo para confirmar as alterações: "))
-
         try:
-            up = UpdateAgreement(status_acordo=status_acordo, data_acordo=data_acordo, acordo_id=acordo_id)
-            up.update()
+            up = self.repo.update_agreement(status_acordo=status_acordo, data_acordo=data_acordo, acordo_id=acordo_id)
+            if up:
+                print("a")
         except Exception as error:
             return "Erro ao atualizar informações do acordo"
         
